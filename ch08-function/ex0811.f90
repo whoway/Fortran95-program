9062c8748dc6de38ed067dd54c1491ad69f5da64
whoway <whoway2017@qq.com>
2022-10-30 16:30:19 +0800

    使用-变量归类-的common变量

---
+++ ch08-function/ex0811.f90
@@ -0,0 +1,30 @@
+program ex0811
+	implicit none
+	  integer :: a, b
+	  common /group1/  a
+	  common /group2/  b
+	  a=1
+	  b=2
+	  call ShowGroup1()
+	  call ShowGroup2()
+	stop
+end program ex0811
+
+
+subroutine ShowGroup1()
+	implicit none
+	  integer :: num1
+	  common /group1/ num1
+	  write(*,*) num1
+	return 
+
+end subroutine ShowGroup1
+
+subroutine ShowGroup2()
+	implicit none
+	  integer :: num1
+	  common /group2/ num1
+	  write(*,*) num1
+	return 
+
+end subroutine ShowGroup2
