c78ed56a4101fe788a646df4dae64b92b81cb253
whoway <whoway2017@qq.com>
2022-10-30 15:00:01 +0800

    Fortran读取字符-注意/的影响

---
+++ ch05-Process-Control-and-Logic-Operation/ex0513.f90
@@ -0,0 +1,26 @@
+program ex0513
+	implicit none
+	  real a,b,ans
+	  character operator
+	  read(*,*) a
+	  read(*,"(A1)") operator !不赋值格式时，有些机器会读不到除号"/"
+	  read(*,*) b
+
+	select case(operator)
+	case('+')
+	  ans=a+b
+	case('-')
+	  ans=a-b
+	case('*')
+	  ans=a*b
+	case('/')
+	  ans=a/b
+	case default !输入其他符号不处理
+	  write(*,"('Unknown operator ',A1)" ) operator
+	  stop !结束程序
+	end select
+
+	write(*,"(F6.2,A1, F6.2,'=',F6.2)") a, operator, b, ans
+	stop
+
+end program ex0513
